ac9cd6b160dc5398087b673a327cb92f75711a27
drewhosick <drewhosick@gmail.com>
2021-08-30 05:40:37 -0400

    New update

--- projects/RNGame/rngame.js
+++ projects/RNGame/rngame.js
@@ -1,22 +1,32 @@
 const guessDisplay = document.querySelector('.theGuess');
 const theResult = document.querySelector('.result');
 const inputGuess = document.querySelector('.inputGuess');
-
-document.getElementById("myBtn").onclick = check;
+const myBtn = document.querySelector('#myBtn');
+const reset = document.querySelector('#reset');
 
 let chosenNumber = Math.round(Math.random() * 100);
 
 function check()
 {
+    check.preventDefault();
     let guess = Number(inputGuess.value);
-    guessDisplay.innerHTML = guess;
+    guessDisplay.innerText = guess;
     
     if (chosenNumber == guess) {
-        theResult.innerHTML = "That was right.  It was " + guess + ".";
+        theResult.innerText = "That was right.  It was " + guess + ".";
     } else if (chosenNumber < guess) {
-        theResult.innerHTML = "Too high. Try again.";
+        theResult.innerText = "Too high. Try again.";
     } else {
-        theResult.innerHTML = "Too low. Try again.";
+        theResult.innerText = "Too low. Try again.";
     }
-    return false;
 }
+
+function reset {
+    let chosenNumber = Math.round(Math.random() * 100);
+    theResult.innerHTML = 'Game reset.  New number chosen';
+    theResult.innerHTML = '';
+}
+
+myBtn.addEventListener("click", check);
+reset.addEventListener("click", reset);
+
